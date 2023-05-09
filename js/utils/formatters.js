0181dea6b14263a5127c1093587dbbb8dd36a659
Alina <AliKochneva@yandex.ru>
2023-05-09 14:39:21 +0700

    currency formatter

--- js/utils/formatters.js
+++ js/utils/formatters.js
@@ -3,13 +3,11 @@ const priceFormatter = new Intl.NumberFormat('ru-RU', {
     currency: 'RUB',
     maximumFractionDigits: 0
 })
-// 7 000 000 Р
 
 const priceFormatterDecimals = new Intl.NumberFormat('ru-RU', {
 	style: 'currency',
 	currency: 'RUB',
 	maximumFractionDigits: 2,
 });
-// 7 000 000.45 Р
 
 export {priceFormatter, priceFormatterDecimals}
